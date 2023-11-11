0b7be44701f64f11d533a6f553e862c0f547977d
LucaBla <me@lucablazevic.dev>
2023-11-11 19:37:50 +0100

    add features to set weights and speed

--- src/main.js
+++ src/main.js
@@ -3,26 +3,105 @@ import { Clock } from 'three';
 
 const clock = new Clock();
 
+const actionWeightInputs = document.getElementsByClassName("weight-input");
+
+const startBtn = document.getElementById("start-btn");
+
+const container = document.querySelector('#scene-container');
+
+const speedSlider = document.getElementById("speed-slider");
+const sliderValueDisplay = document.getElementById("speed-slider-value");
+
+let enteredWeights = [];
+let animationSpeed = 1;
+
 async function main() {
   // Get a reference to the container element
-  const container = document.querySelector('#scene-container');
 
   // create a new world
   const world = new World(container);
 
+  
   // draw the scene
   world.render();
-
+  
   await world.init();
   animate(world);
+
+  initializeEnteredWeights();
+
+  initializeEventListener(world);
 }
 
 function animate(world){
   world.getControls().update();
-  world.getMixer().update(clock.getDelta());
+  if(world.getMixer() != undefined){
+    world.getMixer().update(clock.getDelta());
+  }
   world.render();
 
   requestAnimationFrame(() => animate(world));
+}
+
+function initializeEventListener(world){
+  startBtn.addEventListener("click", () => {
+    const weightSum = sumUpWeights();
+    if(weightSum >100 || weightSum < 99){
+      alert("The sum of the weights must be 100 or 99!");
+    }
+    else{
+      world.handleStartBtnClick(enteredWeights, animationSpeed);
+    }
+  });
+
+  speedSlider.addEventListener("input", () => {
+    sliderValueDisplay.innerHTML = speedSlider.value + "x";
+    animationSpeed = parseFloat(speedSlider.value);
+  });
+
+  for(let i=0; i < actionWeightInputs.length; i++){
+    actionWeightInputs[i].addEventListener("input", () =>{
+      if(actionWeightInputs[i].value > 100){
+        actionWeightInputs[i].value = 100;
+      }
+      else if(actionWeightInputs[i].value < 0){
+        actionWeightInputs[i].value = 0;
+      }
+
+      while(actionWeightInputs[i].value.length > 1 && actionWeightInputs[i].value[0] == '0'){
+        actionWeightInputs[i].value = actionWeightInputs[i].value.slice(1);
+      }
+
+      if(actionWeightInputs[i].value === ""){
+        actionWeightInputs[i].value = 0;
+      }
+
+      //actionWeightInputs[i].value = parseFloat(actionWeightInputs[i].value);
+      enteredWeights[i] = parseFloat(actionWeightInputs[i].value);
+      updateUnassignedWeights();
+    })
+  }
+}
+
+function initializeEnteredWeights(){
+  for(let i=0; i < actionWeightInputs.length; i++){
+    enteredWeights.push(0);
+  }
+}
+
+function sumUpWeights(){
+  let sum = 0;
+
+  for (let i = 0; i < enteredWeights.length; i++) {
+    sum += enteredWeights[i];
+  }
+
+  return sum;
+}
+
+function updateUnassignedWeights(){
+  let unassignedWeightsDisplay = document.getElementById("unassigned-weights");
+  unassignedWeightsDisplay.innerHTML = (100 - sumUpWeights()).toFixed(1) + "%";
 }
 
 
--- src/world/world.js
+++ src/world/world.js
@@ -9,6 +9,7 @@ import { loadMen, menLoadingManager, loadAnimation} from "./systems/fbxLoader.js
 import { createRenderer } from './systems/renderer.js';
 import { Resizer } from './systems/resizer.js';
 
+let model;
 let scene;
 let camera;
 let renderer;
@@ -34,30 +35,13 @@ class World {
   }
 
   async init(){
-    const men = await loadMen();
-
-    let animations = await this.createAnimationsArray(
-      [
-        'assets/animations/rightHook.fbx',
-        'assets/animations/handRaising.fbx',
-        'assets/animations/fistPump.fbx'
-      ]);
-      let actionsArray = [];
-
-    this.normalizeTimeTracks(animations);
-    
-    mixer = new AnimationMixer(men);
-
-    actionsArray = this.clipActions(mixer, animations);
-    this.setupActions(actionsArray);
-
-    console.log(animations);
+    model = await loadMen();
 
     menLoadingManager.onLoad = this.render;
 
-    men.position.set(0,0,3);
-    men.scale.set(.01,.01,.01);
-    scene.add(men);
+    model.position.set(0,0,3);
+    model.scale.set(.01,.01,.01);
+    scene.add(model);
     this.render();
   }
 
@@ -74,34 +58,60 @@ class World {
     return mixer;
   }
 
-  normalizeTimeTracks(actionArray){
+  async initializeAnimations(model, weights, animationSpeed){
+    let animations = await this.createAnimationsArray(
+      [
+        'assets/animations/rightHook.fbx',
+        'assets/animations/handRaising.fbx',
+        'assets/animations/fistPump.fbx'
+      ]);
+      let actionsArray = [];
+
+      this.normalizeTimeTracks(animations, weights);
+
+      mixer = new AnimationMixer(model);
+
+      actionsArray = this.clipActions(mixer, animations);
+      this.setupActions(actionsArray, weights, animationSpeed);
+  }
+
+  normalizeTimeTracks(actionArray, weights){
     //find biggest timeTrack
     //set all timeTracks in both to biggest timeTrack
-    let biggestTimeTrack = this.findBiggestTimeTrack(actionArray);
+    //or scale all timeTracks based on biggest timeTrack
+    let biggestTimeTrack = this.findBiggestTimeTrack(actionArray, weights);
     console.log(biggestTimeTrack);
 
-    actionArray.forEach(action => {
-      action.tracks.forEach(track => {
+    for(let i = 0; i < actionArray.length; i++){
+      if(weights[i] == 0){
+        continue;
+      }
+
+      actionArray[i].tracks.forEach(track => {
         const scaleFactor = biggestTimeTrack.length / track.times.length;
   
         track.times = track.times.map(time => time * scaleFactor);
       });
       
-      action.duration = biggestTimeTrack[biggestTimeTrack.length - 1];
-    });
+      actionArray[i].duration = biggestTimeTrack[biggestTimeTrack.length - 1];
+    }
   }
 
-  findBiggestTimeTrack(actionArray){
+  findBiggestTimeTrack(actionArray, weights){
     let biggestTimeTrack = null;
 
-    actionArray.forEach(action => {
-      action.tracks.forEach(track => {
+    for(let i = 0; i < actionArray.length; i++){
+      if(weights[i] == 0){
+        continue;
+      }
+
+      actionArray[i].tracks.forEach(track => {
         if(biggestTimeTrack == null || 
            track.times.length > biggestTimeTrack.length){
             biggestTimeTrack = track.times;
         }
       });
-    });
+    }
 
     return biggestTimeTrack;
   }
@@ -129,12 +139,24 @@ class World {
     return actionArray;
   }
 
-  setupActions(actionArray){
-    actionArray.forEach(action => {
-      action.setLoop(LoopRepeat, Infinity);
-      action.setEffectiveWeight(0.5);
-      action.play();
-    });
+  setupActions(actionArray, weights, animationSpeed){
+    for(let i = 0; i < actionArray.length; i++){
+      actionArray[i].setLoop(LoopRepeat, Infinity);
+      actionArray[i].setEffectiveWeight(weights[i]);
+      actionArray[i].setEffectiveTimeScale(animationSpeed);
+      actionArray[i].play();
+    }
+  }
+
+  handleStartBtnClick(enteredWeights, animationSpeed){
+    console.log(enteredWeights);
+    for(const number of enteredWeights){
+      if(isNaN(number)){
+        alert("Please enter valid numbers!");
+        return;
+      }
+    }
+    this.initializeAnimations(model, enteredWeights, animationSpeed);
   }
 }
 
