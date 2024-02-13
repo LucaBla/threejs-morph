4d5e99a592de61a10c81e9deb8f6f4a188c29b34
LucaBla <me@lucablazevic.dev>
2024-02-13 14:21:51 +0100

    comment animationMorpher.js

--- src/world/systems/animationMorpher.js
+++ src/world/systems/animationMorpher.js
@@ -35,6 +35,8 @@ async function morphAnimations(fileArray, weights, model, isHipsRotationLocked){
 }
 
 function weightedAverageQuaternions(quaternions, weights) {
+  // calculates the weighted average of the given quaternions
+  // each quaternion is multiplied by its corresponding weight
   if (quaternions.length === 0 || 
       weights.length !== quaternions.length) {
     console.error('Ungültige Eingabeparameter');
@@ -47,6 +49,7 @@ function weightedAverageQuaternions(quaternions, weights) {
   let forwardSum = new Vector3();
   let upwardSum = new Vector3();
 
+  // calculate the weighted sums of forward and upward vectors
   for (let i = 0; i < count; i++) {
     const weightedForward = new Vector3(0, 0, 1).applyQuaternion(quaternions[i]).multiplyScalar(weights[i]);
     const weightedUpward = new Vector3(0, 1, 0).applyQuaternion(quaternions[i]).multiplyScalar(weights[i]);
@@ -55,15 +58,16 @@ function weightedAverageQuaternions(quaternions, weights) {
     upwardSum.add(weightedUpward);
   }
 
+  // calculate the average of the weighted sums
   forwardSum.divideScalar(count);
   upwardSum.divideScalar(count);
 
   const resultQuaternion = new Quaternion();
   const matrix = new Matrix4();
 
-  //Create a rotation matrix 
-  //that aligns the object to the forward vector, 
-  //with upward as the orientation
+  // Create a rotation matrix 
+  // that aligns the object to the forward vector, 
+  // with upward as the orientation
   matrix.lookAt(forwardSum, new Vector3(), upwardSum);
 
   //set the quaternion based on the rotation matrix
@@ -73,6 +77,8 @@ function weightedAverageQuaternions(quaternions, weights) {
 }
 
 function weightedAverageVector3(vectors, weights){
+  // calculates the weighted average of the given Vectors
+  // each Vector is multiplied by its corresponding weight
   if (vectors.length === 0 || vectors.length !== weights.length) {
     console.log(vectors.length);
     console.log(weights.length);
@@ -82,6 +88,7 @@ function weightedAverageVector3(vectors, weights){
 
   let weightedSum = new Vector3();
 
+  // calculate the weighted vectors and sums them up
   for (let i = 0; i < vectors.length; i++) {
     const weightedVector = vectors[i].clone().multiplyScalar(weights[i]);
     weightedSum.add(weightedVector);
@@ -90,6 +97,7 @@ function weightedAverageVector3(vectors, weights){
   const totalWeight = weights.reduce((sum, weight) => sum + weight, 0);
 
   if (totalWeight !== 0) {
+    // returns the average of all the weighted vectors
     const average = weightedSum.divideScalar(totalWeight);
     return average;
   } else {
@@ -99,65 +107,66 @@ function weightedAverageVector3(vectors, weights){
 }
 
 function combineAnimations(weights){
+  // combines the given animations by combining their KeyFrameTracks
   const tracksToCombineList = getTracksToCombineList();
 
+  // iterarte throughtracks with the same name to combine them
   for(const sameNamedTracks of tracksToCombineList) {
+    // extract name and type of the track
     const trackName = findFirstNameInTrackList(sameNamedTracks);
     const trackType = trackName.split('.')[1];
-      //elements meaning quaternion or vector3
-      const elementsTracksArrays = [];
+    
+    //elements meaning either quaternion or vector3
+    const elementsTracksArrays = [];
 
-      sameNamedTracks.forEach(track => {
-        if(track !== null){
-          let element;
-          if(trackType === 'quaternion'){
-            element = getQuaternionsFromValuesArray(track.values);
-          }
-          else if(trackType === 'position' || trackType === 'scale'){
-            element = getVectorsFromValuesArray(track.values);
-          }
-          else{
-            console.error('Invalid Type!');
-          }
-
-          elementsTracksArrays.push(element);
+    // converting the KeyFrameTracks of tracks with the same name to
+    // element(quaternion/vector) arrays and then adding these arrays 
+    // to the elementsTracksArrays
+    sameNamedTracks.forEach(track => {
+      if(track !== null){
+        let elementArray;
+        if(trackType === 'quaternion'){
+          elementArray = getQuaternionsFromValuesArray(track.values);
+        }
+        else if(trackType === 'position' || trackType === 'scale'){
+          elementArray = getVectorsFromValuesArray(track.values);
         }
         else{
-          elementsTracksArrays.push(null);
-        }
-      })
-
-      let averagedElementsArray = 
-        weightedAverageElementArray(elementsTracksArrays, weights);
-      
-        const newValuesArray = [];
-
-        for(const element of averagedElementsArray){
-          newValuesArray.push(
-            ...elementToArray(element)
-          );
+          console.error('Invalid Type!');
         }
 
-        if(findTrackInBaseAnimations(trackName)){
-          exchangeTrackValuesInBaseAnimation(newValuesArray, trackName);
-        }
-        else{
-        //track could not be found in baseAnimation
-        addTrackToBaseAnimation(trackName, trackType, newValuesArray);
-        }
-  }
-}
+        elementsTracksArrays.push(elementArray);
+      }
+      else{
+        elementsTracksArrays.push(null);
+      }
+    })
 
-function getTypeFromTrack(track){
-  if(track instanceof VectorKeyframeTrack){
-    return Vector3.constructor;
-  }
-  else if(track instanceof QuaternionKeyframeTrack){
-    return Quaternion.constructor;
-  }
-  else{
-    console.error("Invalid Type!");
-    return null;
+    // calculates the weighted average of the elementsTracksArrays
+    let averagedElementsArray = 
+      weightedAverageElementArray(elementsTracksArrays, weights);
+    
+    const newValuesArray = [];
+
+    // convert the weighted averaged elements
+    // from the averagedElementsArray to an array
+    // to make them compatible with KeyFrameTracks
+    for(const element of averagedElementsArray){
+      newValuesArray.push(
+        ...elementToArray(element)
+      );
+    }
+
+    if(findTrackInBaseAnimations(trackName)){
+      // exchanges the values of a given track
+      // with the calculated weighted averages
+      exchangeTrackValuesInBaseAnimation(newValuesArray, trackName);
+    }
+    else{
+      // track could not be found in baseAnimation
+      // so a new one will be created
+      addTrackToBaseAnimation(trackName, trackType, newValuesArray);
+    }
   }
 }
 
@@ -170,6 +179,7 @@ function findFirstNameInTrackList(trackList){
 }
 
 function addTrackToBaseAnimation(trackName, trackType, newValues){
+  // adds a new track to the baseAnimation
   let newTrack;
 
   if(trackType === 'quaternion'){
@@ -201,6 +211,8 @@ function addTrackToBaseAnimation(trackName, trackType, newValues){
 }
 
 function exchangeTrackValuesInBaseAnimation(newValues, trackName){
+  // exchanges the values of a track in the baseAnimation
+  // with the given values
   let foundTrack = findTrackInBaseAnimations(trackName);
     if(foundTrack != null){
       findTrackInBaseAnimations(trackName).values = newValues;
@@ -208,6 +220,7 @@ function exchangeTrackValuesInBaseAnimation(newValues, trackName){
 }
 
 function elementToArray(element){
+  // converts a element of type quaternion or Vector3 to an array
   if(element instanceof Quaternion){
     return quaternionToArray(element);
   }
@@ -246,10 +259,12 @@ function vector3ToArray(vector3){
 }
 
 function getTracksToCombineList(){
+  // searches through all the animations that should be combined
+  // returns an array of arrays combining all the
+  // tracks with the same name (meaning these should be combined)
   const tracksToCombineList = [];
   let sameNamedTracks = [];
 
-  //find all the tracks with the same name
   trackNameList.forEach(trackName =>{
     animationsArray.forEach(animation =>{
       let searchedTrack = findTrackInAnimation(animation, trackName);
@@ -280,26 +295,41 @@ function findFirstLengthInElementLists(elementList){
 }
 
 function weightedAverageElementArray(elementArray, weights){
+  // returns an array containing the weighted averaged elements
+  // of the given elementArray
+  // elementArray is an array of arrays containing elements (q/v3)
+  // every array in the elementArray represents a KeyFrameTrack ValueArray
+
+  // weightsArray should not be modified during the calculations
+  // otherwiseweights will be missing if the algortithm is started again
   const tempWeights = [...weights];
+
+  // find the type (Quaternion/Vector3) of the elements in the array
   const type = findFirstTypeInElementList(elementArray);
 
   if(elementArray.length > 0){
+    // array to store elements with the same index 
+    // from different arrays
     const sameIndexElements = [];
+
     const averagedElementsArrays = [];
 
-    //the size of all the element arrays in elementArray
+    // determine the length of each array in the elementArray
     const elementsArrayElementLength = 
       findFirstLengthInElementLists(elementArray);
 
+      // find indexes to remove if an element is null in elementArray
       const indexesToRemove = [];
       for(let i = 0; i < elementsArrayElementLength; i++){
         if(elementArray[i] === null){
-          //this track had no element with this name
-          //so its removed because it cant be used to calculate the average
+          // track had no element with this index 
+          // so it is removed from calculations
           indexesToRemove.push(i);
         }
       }
       
+      // remove elements and corresponding weights
+      // for indexes marked for removal
       for (let i = indexesToRemove.length - 1; i >= 0; i--) {
         const index = indexesToRemove[i];
         elementArray.splice(index, 1);
@@ -307,10 +337,16 @@ function weightedAverageElementArray(elementArray, weights){
       }
     
     for(let i = 0; i < elementsArrayElementLength; i++){
+      // gather elements with the same index from different arrays
+      // inside elementArray
       for(let j = 0; j < elementArray.length; j++){
         sameIndexElements.push(elementArray[j][i]);
       }
+
       let weightedElement;
+
+      // calculate weighted average
+      // based on element type (Quaternion or Vector3)
       if(type === Quaternion){
         weightedElement = weightedAverageQuaternions(
           sameIndexElements, tempWeights)
@@ -327,11 +363,14 @@ function weightedAverageElementArray(elementArray, weights){
         weightedElement
       );
 
+      // clear the sameIndexElements array for the next iteration
       sameIndexElements.length = 0;
     }
 
     return(averagedElementsArrays);
   }
+
+  // handle the case where the elementArray is empty
   console.error("Could not weight Quaternion Array!");
   return null;
 }
@@ -388,14 +427,16 @@ function normalizeKeyFrameTrackValueArrays(){
 }
 
 function normalizeTracksValueArray(track, timesArray){
+  // normalizes the values array of a KeyFrameTrack
+  // normalizing means adkusting the array to the given times array
+  // quaternion value arrays need to be x4 the size of times array
+  // vector3 needs to be x3
   if(!(track instanceof VectorKeyframeTrack || 
   track instanceof QuaternionKeyframeTrack)){
     console.error("Track is not of type Quaternion or VectorKeyframeTrack");
     return null
   }
 
-  console.log(track.name);
-  console.log(track.times.length);
   if(track.values.length === 0){
     console.error(`Track${track.name} values array is empty!`);
     return track.values;
@@ -403,58 +444,64 @@ function normalizeTracksValueArray(track, timesArray){
 
   let valueArray = track.values;
   let newArray = [];
-  let componentSize;
+  let elementSize;
   let newLength;
-  let componentsArray = [];
-  let newComponentsArray = [];
+  let elementsArray = [];
+  let newElementsArray = [];
 
+  // determines the size of an element based on track type
+  // and converts the value arrays into elementsArrays
   if(track instanceof QuaternionKeyframeTrack){
-    componentSize = 4;
-    componentsArray = getQuaternionsFromValuesArray(valueArray);
+    elementSize = 4;
+    elementsArray = getQuaternionsFromValuesArray(valueArray);
   }
   else if(track instanceof VectorKeyframeTrack){
-    componentSize = 3;
-    componentsArray = getVectorsFromValuesArray(valueArray);
+    elementSize = 3;
+    elementsArray = getVectorsFromValuesArray(valueArray);
   }
 
-  newLength = timesArray.length * componentSize;
+  // calculates the desired length of the normalized values array
+  newLength = timesArray.length * elementSize;
 
+  // check if the values array is already of the correct size
   if(valueArray.length === newLength){
-    //Track is the right size
     track.times = baseAnimation.tracks[0].times;
     return valueArray;
   }
 
-  if(valueArray.length <= componentSize){
-    //the array only contains one object
-    //double it so the function can then fill it
-    componentsArray = componentsArray.concat(componentsArray);
-    //return valueArray;
+  // check if the values array is smaller than the element size
+  if(valueArray.length <= elementSize){
+    // the array only contains one object
+    // double it so the function can then fill it
+    elementsArray = elementsArray.concat(elementsArray);
   }
 
-  for(let i = 0; i < componentsArray.length; i++){
-    newComponentsArray.push(componentsArray[i]);
+  for(let i = 0; i < elementsArray.length; i++){
+    newElementsArray.push(elementsArray[i]);
 
-    if(newComponentsArray.length * componentSize === 
+    // check if the new elements array has reached the desired length
+    if(newElementsArray.length * elementSize === 
         newLength){
           break;
     }
 
-    if( i === componentsArray.length - 1){
-      //stop the for-loop
-      //because there cant be a average quaternion between
-      //the last quaternion and a non existing one
+    // check if the current iteration is the last element in the array
+    if( i === elementsArray.length - 1){
+      // Stop the loop 
+      // since there cannot be an average between the last 
+      // and a non-existing element
       break;
     }
 
+    // interpolate values based on track type
     if(track instanceof QuaternionKeyframeTrack){
       let newQuaternion = new Quaternion;
   
-      newComponentsArray.push(
+      newElementsArray.push(
         //average of two quaternions
         newQuaternion.slerpQuaternions(
-          componentsArray[i], 
-          componentsArray[i + 1], 
+          elementsArray[i], 
+          elementsArray[i + 1], 
           .5
         )
       );
@@ -462,24 +509,27 @@ function normalizeTracksValueArray(track, timesArray){
     else if(track instanceof VectorKeyframeTrack){
       let newVector3 = new Vector3;
 
-      newComponentsArray.push(
+      newElementsArray.push(
       //average of two vectors
       newVector3.lerpVectors(
-        componentsArray[i], 
-        componentsArray[i + 1], 
+        elementsArray[i], 
+        elementsArray[i + 1], 
         .5
       )
     );
     }
 
-    if(newComponentsArray.length * componentSize === 
+    // check if the new elements array has reached the desired length
+    if(newElementsArray.length * elementSize === 
       newLength){
         break;
     }
   }
 
+  // convert the new elements array 
+  // to a flat array of values based on track type
   if(track instanceof QuaternionKeyframeTrack){
-    for(const quaternion of newComponentsArray){
+    for(const quaternion of newElementsArray){
       newArray.push(
         quaternion.x, 
         quaternion.y, 
@@ -489,7 +539,7 @@ function normalizeTracksValueArray(track, timesArray){
     }
   }
   else if(track instanceof VectorKeyframeTrack){
-    for(const vector of newComponentsArray){
+    for(const vector of newElementsArray){
       newArray.push(
         vector.x, 
         vector.y, 
@@ -498,15 +548,19 @@ function normalizeTracksValueArray(track, timesArray){
     }
   }
 
+  // check if the new array is of the correct length
   if(newArray.length === newLength){
     track.times = baseAnimation.tracks[0].times;
     return newArray;
   }
   else if(newArray.length < newLength){
+    // new array is smaller than the desired length 
+    // update track values and run function recursively
     track.values = newArray;
     return normalizeTracksValueArray(track, timesArray);
   }
   else{
+    // new array is larger than the desired length
     console.error("ERROR size is: " + newArray.length + " Should be: " + newLength);
     return null;
   }
@@ -514,7 +568,8 @@ function normalizeTracksValueArray(track, timesArray){
 }
 
 function fillTrackNameList(){
-  //fill the list with all track names
+  // fill the list with the names of all the tracks
+  // of the animations that should be combined
   animationsArray.forEach(animation =>{
     animation.tracks.forEach(track =>{
       if(!trackNameList.includes(track.name)){
@@ -525,6 +580,8 @@ function fillTrackNameList(){
 }
 
 function getVectorsFromValuesArray(vectorValues){
+  // receives an array of values representing multiple Vector3s
+  // and returns an array of Vector3 elements
   let vectorArray =[];
 
   for (let i = 0; i < vectorValues.length; i += 3) {
@@ -539,6 +596,8 @@ function getVectorsFromValuesArray(vectorValues){
 }
 
 function getQuaternionsFromValuesArray(quaternionValues){
+  // receives an array of values representing multiple quaternions
+  // and returns an array of quaternion elements
   let quaternionArray =[];
 
   for (let i = 0; i < quaternionValues.length; i += 4) {
@@ -554,6 +613,8 @@ function getQuaternionsFromValuesArray(quaternionValues){
 }
 
 async function createAnimationsArray(fileArray, model){
+  // receives the fileArray, loads the animations from these files
+  // and returns an array of said animations
   let animationsArray = [];
 
   for (const animationFile of fileArray) {
